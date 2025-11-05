16c6cf533b6398d93aaa97c050d2c76f98df3bcf
iMprov <improv00@mail.ru>
2025-11-05 09:48:48 +0700

    Добавлена страница About и пройдены тесты

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,18 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
+  test "обращаемся к home" do
     get static_pages_home_url
     assert_response :success
   end
 
-  test "should get help" do
+  test "обращаемся к help" do
     get static_pages_help_url
+    assert_response :success
+  end
+
+  test "обращаемся к about" do
+    get static_pages_about_url
     assert_response :success
   end
 end
