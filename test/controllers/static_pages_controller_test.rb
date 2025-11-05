10d3e49f1e18025f91a931fa17e2befa7e42fdda
iMprov <improv00@mail.ru>
2025-11-05 11:12:02 +0700

    Добавлен метод в тест для заголовка

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,28 @@
 require "test_helper"
 
+
+
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    "Ruby on Rails Tutorial Sample App"
+  end 
+
   test "обращаемся к home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Home | #{setup}"
   end
 
   test "обращаемся к help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | #{setup}"
   end
 
   test "обращаемся к about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | #{setup}"
   end
 end
